172d308fe4a22e241956dc8401fb18c34dc07ebb
DESKTOP-9R3V8HE\profesor <csi23-jpribio@colegioaltair.net>
2023-10-04 10:06:51 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace eduproyectoprueba
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            //FOR
+            /* string nombre;
+             for (int i = 1; i < 20; i++) {
+                  nombre = "Jaime";
+                 Console.WriteLine(nombre);
+             }*/ //La mejor forma y la correcta.
+
+            //foreach sirve para ahcer listas de elementos
+
+          
+
+
+        }
+    }
+}
